a85616cc02315b5cee0b53359076df1fee0d3b28
digrv <diana.gorovaya2003@mail.ru>
2025-10-24 17:43:48 +0700

    Создание проекта

---
+++ DriveApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DriveApp
+//
+//  Created by Диана Горовая on 24.10.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DriveApp/DriveAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DriveAppApp.swift
+//  DriveApp
+//
+//  Created by Диана Горовая on 24.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct DriveAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
